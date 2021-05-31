26781518bf39b81611e37badf3a57894b2f80dca
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-05-31 20:14:35 +0700

    update

--- client/human_resource/src/App.js
+++ client/human_resource/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./component/Login2";
+import Home from "./component/Home";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <div className="main">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
