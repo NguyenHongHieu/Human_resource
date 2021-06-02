1ace01eba83c3b7c9c770e6c0f1dd7edf65a0d06
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-06-02 20:08:25 +0700

    update pie chart

--- client/human_resource/src/component/Home/index.js
+++ client/human_resource/src/component/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "../Header";
+import DemoPie from '../Piechart';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <h1>Home</h1>
+      <DemoPie />
     </div>
   )
 }
